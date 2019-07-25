dcac321e7612a03c8d6e2f0c3c6e71fe7d227393
Mayra Azevedo <mayradazevedo@ufrn.edu.br>
2019-07-25 12:44:24 -0300

    fix #1, refatora classe para atender aos padroes do PEP

--- odufrn-downloader/ODUFRNDownloader.py
+++ odufrn-downloader/ODUFRNDownloader.py
@@ -5,15 +5,35 @@ import os
 
 
 class ODUFRNDownloader():
+    """Classe responsável pelo download de datasets.
+
+        Atributos
+        ---------
+        base: str
+            a url para a API de dados abertos da UFRN
+        action: str
+            a url para a página de ações da API
+        dataset: str
+            a url para a consulta de datasets da API da UFRN
+
+        Métodos
+        -------
+        list_package()
+            Lista os conjuntos de dados.
+
+        download_package(name, path=os.getcwd())
+            Exibe conjunto de dado de acordo com seu nome
+            e baixa-os em pastas com o nome do respectivo
+            conjunto de dado
+    """
+
     def __init__(self):
         self.base = 'http://dados.ufrn.br/'
         self.action = self.base + 'api/action/'
         self.dataset = self.base + 'api/rest/dataset/'
 
-    def listPackage(self):
-        """
-        Lista os conjuntos de dados
-        """
+    def list_package(self):
+        """Lista os conjuntos de dados. """
         try:
             print(
                 json.dumps(requests.get(self.action + 'package_list').json(), indent=4)
@@ -22,13 +42,21 @@ class ODUFRNDownloader():
             print(e)
             sys.exit(1)
 
-    def downloadPackage(self, name, path=os.getcwd()):
+    def download_package(self, name, path=os.getcwd()):
         """
         Exibe conjunto de dado de acordo com seu nome
         e baixa-os em pastas com o nome do respectivo
         conjunto de dado
 
         >>> Ex. downloadPackage('acervo-biblioteca')
+
+        Parâmetros
+        ----------
+        name: str
+            nome do dataset
+        path: str
+            o caminho da pasta onde serão adicionados os arquivos
+            (por padrão, a pasta atual)
         """
 
         dataset = requests.get(self.dataset + name).json()
