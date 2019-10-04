56a9ec901dd4bcfac9b6ab77e1f9bcf402a66533
João Pedro Limão <jplimao12@gmail.com>
2019-10-04 12:44:01 -0300

    Transformando o notebook em um script

---
+++ examples/dinheiro_total_obras_2018.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+'''
+Objetivo: Nesse script em questão queremos descobrir a quantidade de dinheiro gasto 
+em licitações de obras em 2018 pela Universidade Federal do Rio Grande do Norte (UFRN). 
+
+Requisitos: Pandas, ODUFRNDownloader
+
+Use o comando a seguir para instalar as dependências
+pip install pandas odufrn-downloader
+'''
+
+import pandas as pd
+from odufrn_downloader import ODUFRNDownloader
+
+ufrn_data = ODUFRNDownloader()
+ufrn_data.download_package('obras', dictionary=False)
+obras = pd.read_csv('obras/Obras.csv', error_bad_lines=False, sep=';')
+
+obras_2018 = obras[obras['licitacao'].str.contains('2018')]['valor']
+# Removendo R$ e espaços dos dados selecionados
+obras_2018 = obras_2018.str.replace('R', '').str.replace('$', '').str.strip()
+# Substituindo vígula por ponto para converter para float
+obras_2018 = obras_2018.str.replace('.', '').str.replace(',', '.')
+# Transformando os dados de string para float
+obras_2018 = obras_2018.astype(float)
+
+print('O gasto da UFRN para obras em 2018 foi: R$ ' + '{0:.2f}'.format(obras_2018.sum()))
